627c430e881c9ebc67fd2d19512307d49a0a60c7
Admin <Admin@FOREVEREST>
2022-10-17 22:34:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,54 @@
+﻿int n = 7;
+
+Random rnd = new Random();
+
+int[] arr = new int[rnd.Next(10, 20)];
+
+for (int i = 0; i < n; i++)
+{
+    arr[i] = rnd.Next(1, 20);
+    Console.Write($"{arr[i]}" + "\t");
+}
+Console.WriteLine();
+
+
+int max = 0;
+int min = 10000;
+for (int i = 0; i < n; i++)
+{
+    if (max < arr[i])
+    {
+        max = arr[i];
+    }
+
+    if (min > arr[i])
+    {
+        min = arr[i];
+    }
+}
+
+int sum = 0;
+
+bool a = false;
+for (int i = 0; i < n; i++)
+{
+    if (arr[i] == max)
+    {
+        a = true;
+    }
+    if (arr[i] == min)
+    {
+        a = false;
+    }
+    if (a)
+    {
+        sum += arr[i];
+    }
+}
+if (a)
+{
+    sum = 0;
+}
+else sum -= max;
+
+Console.WriteLine(sum);
